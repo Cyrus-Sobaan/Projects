9367c9a4b33f3f22ea14d2ee577244736dd09c8c
Cyrus-Sobaan <Sobaanqureshi1@gmail.com>
2024-02-05 15:22:23 +0530

    My portfolio

---
+++ Portfolio/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Full Stack Developer", "Web Developer", "App Developer"],
+    typeSpeed: 50,
+    backSpeed: 10,
+    backDelay: 1000,
+    loop: true
+});
